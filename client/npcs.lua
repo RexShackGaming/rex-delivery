c1f4cf5d7e735a166e99ea592833df691a5923e5
RexShack <rexshackuk@gmail.com>
2025-08-10 11:25:05 +0100

    Initial commit

---
+++ client/npcs.lua
@@ -0,0 +1,59 @@
+local spawnedPeds = {}
+
+CreateThread(function()
+    while true do
+        Wait(500)
+        for k,v in pairs(Config.DeliveryLocations) do
+            local playerCoords = GetEntityCoords(PlayerPedId())
+            local distance = #(playerCoords - v.npccoords.xyz)
+
+            if distance < Config.DistanceSpawn and not spawnedPeds[k] then
+                local spawnedPed = NearPed(v.npcmodel, v.npccoords, v.trader)
+                spawnedPeds[k] = { spawnedPed = spawnedPed }
+            end
+            
+            if distance >= Config.DistanceSpawn and spawnedPeds[k] then
+                if Config.FadeIn then
+                    for i = 255, 0, -51 do
+                        Citizen.Wait(50)
+                        SetEntityAlpha(spawnedPeds[k].spawnedPed, i, false)
+                    end
+                end
+                DeletePed(spawnedPeds[k].spawnedPed)
+                spawnedPeds[k] = nil
+            end
+        end
+    end
+end)
+
+function NearPed(npcmodel, npccoords, trader)
+    RequestModel(npcmodel)
+    while not HasModelLoaded(npcmodel) do
+        Citizen.Wait(50)
+    end
+    spawnedPed = CreatePed(npcmodel, npccoords.x, npccoords.y, npccoords.z - 1.0, npccoords.w, false, false, 0, 0)
+    SetEntityAlpha(spawnedPed, 0, false)
+    Citizen.InvokeNative(0x283978A15512B2FE, spawnedPed, true)
+    SetEntityCanBeDamaged(spawnedPed, false)
+    SetEntityInvincible(spawnedPed, true)
+    FreezeEntityPosition(spawnedPed, true)
+    SetBlockingOfNonTemporaryEvents(spawnedPed, true)
+    SetPedCanBeTargetted(spawnedPed, false)
+    SetPedFleeAttributes(spawnedPed, 0, false)
+    if Config.FadeIn then
+        for i = 0, 255, 51 do
+            Citizen.Wait(50)
+            SetEntityAlpha(spawnedPed, i, false)
+        end
+    end
+    return spawnedPed
+end
+
+-- cleanup
+AddEventHandler("onResourceStop", function(resourceName)
+    if GetCurrentResourceName() ~= resourceName then return end
+    for k,v in pairs(spawnedPeds) do
+        DeletePed(spawnedPeds[k].spawnedPed)
+        spawnedPeds[k] = nil
+    end
+end)
